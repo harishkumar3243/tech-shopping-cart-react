5f1d63ffb588f3737c37354045b53dcb10cab85e
Harish Kumar <85731336+harishkumar3243@users.noreply.github.com>
2023-12-19 13:54:03 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.webp";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.webp";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.webp";
+import product9 from "./assets/products/9.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "iQOO 11 5G",
+    price: 54999,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "HP Victus Gaming Laptop",
+    price: 70990,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Digitek GoCAM DAC-002",
+    price: 6999,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "boAt Xtend Smart Watch",
+    price: 1699,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Noise Buds Connect Truly Wireless ",
+    price: 1499,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Marshall Stanmore II Wireless Speaker",
+    price: 34999,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "HP Ink Tank 419 Printer",
+    price: 19490,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "SanDisk Ultra® microSDXC UHS-I Card",
+    price: 489,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Samsung Galaxy Tab S8 | 27.81cm ",
+    price: 50999,
+    productImage: product9,
+  },
+];
